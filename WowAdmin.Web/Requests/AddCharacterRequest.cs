e0722591ba9aab71f03211d4a0fa348b9b8d37a8
Benjamin Noer <benjaminmnoer25@gmail.com>
2024-04-07 21:32:27 +0200

    Added AutoEquip property

--- WowAdmin.Web/Requests/AddCharacterRequest.cs
+++ WowAdmin.Web/Requests/AddCharacterRequest.cs
@@ -21,5 +21,8 @@ namespace WowAdmin.Web.Requests
 
         [FromQuery]
         public required int Class { get; set; }
+
+        [FromQuery]
+        public required bool AutoEquip { get; set; }
     }
 }
